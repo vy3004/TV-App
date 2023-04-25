fcdc5df793901ff2fbe582e00d11221f43740566
Kha Vy <vytrannguyen3004@gmail.com>
2023-04-25 17:17:53 +0700

    fix slider movie

--- tv-app/src/components/SliderMovie/SliderMovie.js
+++ tv-app/src/components/SliderMovie/SliderMovie.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./SliderMovie.css";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import data from "../../data.json";
 import { useStateProvider } from "../../utils/StateProvider";
 import { actionTypes } from "../../utils/Constant";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 export default function SliderMovie() {
   const [, dispatch] = useStateProvider();
@@ -23,42 +22,88 @@ export default function SliderMovie() {
     });
   };
 
-  var settings = {
-    dots: false,
-    infinite: true,
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    pauseOnHover: true,
-  };
-
   return (
-    data && (
-      <div className="slider-container">
-        <div className="slider-title">Popular Movies</div>
-        <Slider {...settings} className="slider-content">
-          {data.map(({ id, image, title, description, rate }) => {
-            return (
-              <div key={id} className="slider-item">
-                <img
-                  src={image}
-                  alt="img"
-                  onClick={() =>
-                    handleSetCurrentMovie({
-                      id,
-                      image,
-                      title,
-                      description,
-                      rate,
-                    })
-                  }
-                />
-              </div>
-            );
-          })}
-        </Slider>
-      </div>
-    )
+    <div className="slider-container">
+      <div className="slider-title">Popular Movies</div>
+      <Carousel
+        className="slider-content"
+        additionalTransfrom={1}
+        arrows
+        autoPlay
+        autoPlaySpeed={2000}
+        centerMode={false}
+        draggable
+        focusOnSelect={false}
+        infinite
+        keyBoardControl
+        minimumTouchDrag={80}
+        pauseOnHover={true}
+        renderArrowsWhenDisabled={false}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={false}
+        responsive={{
+          desktop: {
+            breakpoint: {
+              max: 3000,
+              min: 1200,
+            },
+            items: 6,
+            partialVisibilityGutter: 40,
+          },
+          desktopSm: {
+            breakpoint: {
+              max: 1200,
+              min: 1024,
+            },
+            items: 5,
+            partialVisibilityGutter: 40,
+          },
+          tablet: {
+            breakpoint: {
+              max: 1024,
+              min: 864,
+            },
+            items: 4,
+            partialVisibilityGutter: 30,
+          },
+          tabletSm: {
+            breakpoint: {
+              max: 864,
+              min: 464,
+            },
+            items: 3,
+            partialVisibilityGutter: 30,
+          },
+          mobile: {
+            breakpoint: {
+              max: 464,
+              min: 0,
+            },
+            items: 1,
+            partialVisibilityGutter: 30,
+          },
+        }}
+      >
+        {data.map(({ id, image, title, description, rate }) => {
+          return (
+            <div key={id} className="slider-item">
+              <img
+                src={image}
+                alt="img"
+                onClick={() =>
+                  handleSetCurrentMovie({
+                    id,
+                    image,
+                    title,
+                    description,
+                    rate,
+                  })
+                }
+              />
+            </div>
+          );
+        })}
+      </Carousel>
+    </div>
   );
 }
